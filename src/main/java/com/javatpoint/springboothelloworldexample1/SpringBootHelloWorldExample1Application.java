3e381aec04e9f47411b045f11021d556d60e27a5
anu-7482 <anasuya74482@gmail.com>
2022-08-10 12:17:06 +0530

    first commit

---
+++ src/main/java/com/javatpoint/springboothelloworldexample1/SpringBootHelloWorldExample1Application.java
@@ -0,0 +1,13 @@
+package com.javatpoint.springboothelloworldexample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloWorldExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloWorldExample1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatpoint/springboothelloworldexample1/SpringBootHelloWorldExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatpoint.springboothelloworldexample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloWorldExample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
